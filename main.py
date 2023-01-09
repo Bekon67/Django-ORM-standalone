a445f646e8a7146b0f6f6a1498a1d7910fa0c198
Bekon67 <gmail.com>
2023-01-09 18:41:02 +0300

    Сохранение перед переносом ОС

--- main.py
+++ main.py
@@ -10,3 +10,16 @@ from datacenter.models import Passcard  # noqa: E402
 if __name__ == '__main__':
     # Программируем здесь
     print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
+
+    passcards = Passcard.objects.all()
+    print(passcards)
+
+    passcard = passcards[0]
+    print(f'owner_name: {passcard.owner_name}')
+    print(f'owner_name: {passcard.passcode}')
+    print(f'owner_name: {passcard.created_at}')
+    print(f'owner_name: {passcard.is_active}')
+
+    print(f'Активных пропусков {Passcard.objects.filter(is_active=True).count()}')
+
+
